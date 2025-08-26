75268e22d69151309e1f309ff82ae57c916ef63f
gbarugahare <gbarugahare@idi.co.ug>
2025-08-26 13:13:41 +0300

    Revert "removes .next from .gitignore list"
    This reverts commit 66c634531217cd7d070d60d0db8edf62679112df.

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"zjTmgcsjbJ7Iwl/ec/T4XVDUaHt7ULudBUMhwJbEHHk=\"}"
--- .next/static/chunks/app/test-css/page-c04c26fd59c31fde.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[593,301,138,281,147,983],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
