0c7d9e16265aa7cbac6094b04ac1a64583ae4afa
gbarugahare <gbarugahare@idi.co.ug>
2025-08-27 14:50:26 +0300

    modified butterfly animation

--- next.config.js
+++ next.config.js
@@ -4,16 +4,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com", "localhost", "fortress-app-77248a1b18af.herokuapp.com"],
+    unoptimized: true,
   },
   // Ensure proper asset handling
-  assetPrefix: '',
+  assetPrefix: "",
   // Enable production browser source maps for debugging
   productionBrowserSourceMaps: true,
   // Ensure proper client-side component handling
   experimental: {
     // optimizeCss: true, // Removed due to issues with Vercel deployment
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
