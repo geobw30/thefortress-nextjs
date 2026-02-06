91b7c8f09758670c5a17cd65bd693dec61d8259c
gbarugahare <gbarugahare@idi.co.ug>
2026-02-06 13:31:05 +0300

    fix: display of more on the stories

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
+export const dynamic = "force-dynamic";
 const DashboardPage = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
